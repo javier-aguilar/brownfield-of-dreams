c7b917736d1eef8442b294d498d2652418b6f8d2
Anthony Gomes <code@tonygom.es>
2020-05-06 21:14:47 -0600

    Add GithubService

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,23 +1,18 @@
 class GithubService
-  def limited_number_repos
-    parse_git_data.take(5)
+  def limited_number_repos(token)
+    get_json(token).take(5)
   end
 
   private
 
-  def api_connect
+  def api_connect(token)
     Faraday.new('https://api.github.com') do |conn|
-      conn.request :oauth2, '8fc591925fda4649d5c44e0e559265218b18e07e', token_type: :bearer
-      conn.adapter Faraday.default_adapter
+      conn.authorization :Bearer, token
     end
   end
 
-  def api_return
-    api_connect.get('/user/repos')
+  def get_json(token)
+    response = api_connect(token).get('/user/repos')
+    JSON.parse(response.body, symbolize_names: true)
   end
-
-  def parse_git_data
-    JSON.parse(api_return.body, symbolize_names: true)
-  end
-
 end
