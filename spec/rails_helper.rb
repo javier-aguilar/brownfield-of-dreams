089375fadb17f8127d3f29aeb05198f11463934d
Javier Aguilar <jvr.agx@gmail.com>
2020-05-10 19:59:00 -0600

    Add Webmock & VCR for API testing in RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,3 +39,11 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 end
+
+VCR.configure do |config|
+  config.ignore_hosts '127.0.0.1', 'localhost'
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<YOUTUBE_API_KEY>') { ENV['YOUTUBE_API_KEY'] }
+  config.configure_rspec_metadata!
+end
