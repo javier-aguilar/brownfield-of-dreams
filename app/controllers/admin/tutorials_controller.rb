0c53c7ce76ebc9ed32aacdc605499ffe1ce38fdf
Javier Aguilar <jvr.agx@gmail.com>
2020-05-10 20:03:51 -0600

    Update create method in tutorials

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -3,7 +3,13 @@ class Admin::TutorialsController < Admin::BaseController
     @tutorial = Tutorial.find(params[:id])
   end
 
-  def create; end
+  def create
+    videos = YouTube::Video.by_playlist(playlist_params[:playlist_id])
+    tutorial_info = YouTube::Playlist.info(playlist_params[:playlist_id])
+    tutorial = Tutorial.create(tutorial_info_params(tutorial_info))
+    add_videos_to_tutorial(tutorial, videos)
+    redirect_to admin_dashboard_path
+  end
 
   def new
     @tutorial = Tutorial.new
@@ -27,5 +33,35 @@ class Admin::TutorialsController < Admin::BaseController
 
   def tutorial_params
     params.require(:tutorial).permit(:tag_list)
+  end
+
+  def playlist_params
+    params.require(:tutorial).permit(:playlist_id)
+  end
+
+  def tutorial_info_params(tutorial)
+    { title: tutorial.title,
+      description: tutorial.description,
+      thumbnail: tutorial.thumbnail,
+      playlist_id: tutorial.id }
+  end
+
+  def video_params(video)
+    { title: video.title,
+      description: video.description,
+      thumbnail: video.thumbnail,
+      video_id: video.id }
+  end
+
+  def add_videos_to_tutorial(tutorial, videos)
+    videos.each_with_index do |video, position|
+      video = tutorial.videos.new(video_params(video).merge(position: position))
+      if video.save
+        link = view_context.link_to('View it here', tutorial_path(tutorial))
+        flash[:success] = "Successfully created tutorial. #{link}."
+      else
+        flash[:error] = 'Unable to create tutorial.'
+      end
+    end
   end
 end
