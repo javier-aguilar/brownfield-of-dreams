0e87e054db5b77adf71e66590f02da2ef9ac93f8
Javier Aguilar <jvr.agx@gmail.com>
2020-05-10 20:07:33 -0600

    Add methods to grab playlist data from YouTube API

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -1,11 +1,36 @@
 class YoutubeService
   def video_info(id)
     params = { part: 'snippet,contentDetails,statistics', id: id }
-
     get_json('youtube/v3/videos', params)
   end
 
+  def playlist_info(id)
+    params = { part: 'snippet', id: id }
+    get_json('https://www.googleapis.com/youtube/v3/playlists', params)
+  end
+
+  def playlist_videos(id)
+    params = { part: 'snippet,contentDetails', playlistId: id, maxResults: 40 }
+    data = get_json('youtube/v3/playlistItems', params)
+    items = data[:items]
+    return unless data[:nextPageToken]
+
+    params = params.merge(pageToken: data[:nextPageToken])
+    more_item_results(params, items)
+  end
+
   private
+
+  def more_item_results(params, items)
+    next_page_results = get_json('youtube/v3/playlistItems', params)
+    items += next_page_results[:items]
+    while next_page_results[:nextPageToken]
+      params[:pageToken] = next_page_results[:nextPageToken]
+      next_page_results = get_json('youtube/v3/playlistItems', params)
+      items += next_page_results[:items]
+    end
+    items
+  end
 
   def get_json(url, params)
     response = conn.get(url, params)
