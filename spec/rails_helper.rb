fe1c9551bf15176ea0260f9e6a81950608d284b6
Anthony Gomes <code@tonygom.es>
2020-05-13 07:45:56 -0600

    Update spec/rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.include Capybara::DSL
 end
 
 VCR.configure do |config|
