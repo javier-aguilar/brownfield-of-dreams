b4de7908fc200971bc10b58e7332b61916d4bf6a
Anthony Gomes <code@tonygom.es>
2020-05-12 22:14:49 -0600

    Add email_activate to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   enum role: { default: 0, admin: 1 }
   has_secure_password
+
+  def email_activate
+    self.status = 'active'
+    self.confirm_token = nil
+    save!(:validate => false)
+  end
 end
