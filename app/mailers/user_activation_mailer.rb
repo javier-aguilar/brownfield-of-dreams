0176ed742a20c7f12b3af3d1f1943f98865b2a6f
Anthony Gomes <code@tonygom.es>
2020-05-13 08:46:24 -0600

    Update UserActivationMailer for correct params calls due to errors from typos

--- app/mailers/user_activation_mailer.rb
+++ app/mailers/user_activation_mailer.rb
@@ -1,9 +1,11 @@
 class UserActivationMailer < ApplicationMailer
-  default :from => "no-reply@somedomain.com"
-  layout 'mailer'
+  # default :from => "no-reply@somedomain.com"
+  # layout 'mailer'
 
   def registration_confirmation(user)
-    user_name = user.first_name + " " + user.last_name
-    mail(:to => "#{user_name} <#{user.email}>", :subject => "Brownfield Registration Confirmation")
+    @user = user
+    @user_name = @user.first_name + " " + @user.last_name
+    @user_email = user.email
+    mail(:to => "#{@user_name} <#{@user_email}>", :subject => "Brownfield Registration Confirmation")
   end
 end
