f3d41e69d95a1ca45b3492e382b246b3ebbfa8a7
Anthony Gomes <code@tonygom.es>
2020-05-12 22:07:03 -0600

    Add conditional to check if user has activated their account before logging them in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to dashboard_path
+      if user.status == 'active'
+        session[:user_id] = user.id
+        redirect_to dashboard_path
+      else
+        flash.now[:error] = 'Please activate your account by following the
+        instructions in the account confirmation email'
+      end
     else
       flash[:error] = 'Looks like your email or password is invalid'
       render :new
