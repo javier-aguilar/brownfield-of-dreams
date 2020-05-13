c82c05891cc4758e38931617e5c9495dc30d4a31
Javier Aguilar <jvr.agx@gmail.com>
2020-05-12 19:04:13 -0600

    Add friendships to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, on: create
   validates :first_name, presence: true
   enum role: { default: 0, admin: 1 }
   has_secure_password
