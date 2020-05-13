5c6d69af6e4826e7871b4aa55d7bc4baab0b68c9
Anthony Gomes <code@tonygom.es>
2020-05-12 21:50:35 -0600

    Add registration confirmation method to UserActivationMailer

--- app/mailers/user_activation_mailer.rb
+++ app/mailers/user_activation_mailer.rb
@@ -1,2 +1,7 @@
 class UserActivationMailer < ApplicationMailer
+  default :from => "us@somedomain.com"
+
+   def registration_confirmation(user)
+      mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
+   end
 end
