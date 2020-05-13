a5dd27af75e0244d25ff69be8746f2b28c993369
Javier Aguilar <jvr.agx@gmail.com>
2020-05-13 15:35:07 -0600

    Test for sad path when visiting admin dashboard

--- spec/features/admin/admin_dashboard_spec.rb
+++ spec/features/admin/admin_dashboard_spec.rb
@@ -12,3 +12,12 @@ feature "An admin visiting the admin dashboard" do
     expect(page).to have_css(".admin-tutorial-card", count: 2)
   end
 end
+feature "A user visiting the admin dashboard" do
+  scenario "will get a 404 error" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    expect{ visit admin_dashboard_path }.to raise_error(ActionController::RoutingError)
+  end
+end
