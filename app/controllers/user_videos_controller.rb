3f483ca2536d85aead606a19dc825d7f34dfff31
Javier Aguilar <jvr.agx@gmail.com>
2020-05-13 14:53:15 -0600

    Update bookmark logic to show user a flash message when not logged in

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -2,19 +2,27 @@ class UserVideosController < ApplicationController
   def new; end
 
   def create
-    user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
-      flash[:error] = 'Already in your bookmarks'
-    elsif user_video.save
-      flash[:success] = 'Bookmark added to your dashboard!'
-    end
-
-    redirect_back(fallback_location: root_path)
+    current_user ? add_bookmark : redirect_to_login
   end
 
   private
 
   def user_video_params
     params.permit(:user_id, :video_id)
+  end
+
+  def add_bookmark
+    user_video = UserVideo.new(user_video_params)
+    if current_user.user_videos.find_by(video_id: user_video.video_id)
+      flash[:error] = 'Already in your bookmarks'
+    elsif user_video.save
+      flash[:success] = 'Bookmark added to your dashboard!'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def redirect_to_login
+    flash[:error] = "Please login to save bookmark"
+    redirect_to login_path
   end
 end
