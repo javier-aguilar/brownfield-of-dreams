167104e6642d866accae108db7ea3a505c03f3f6
Anthony Gomes <code@tonygom.es>
2020-05-13 07:42:41 -0600

    Update registration_confirmation with corrected params calls

--- app/mailers/user_activation_mailer.rb
+++ app/mailers/user_activation_mailer.rb
@@ -2,7 +2,8 @@ class UserActivationMailer < ApplicationMailer
   default :from => "no-reply@somedomain.com"
   layout 'mailer'
 
-   def registration_confirmation(user)
-      mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
-   end
+  def registration_confirmation(user)
+    user_name = user.first_name + " " + user.last_name
+    mail(:to => "#{user_name} <#{user.email}>", :subject => "Brownfield Registration Confirmation")
+  end
 end
