26d41905ee6d723a72d640523f5b9183fe43eccf
Anthony Gomes <code@tonygom.es>
2020-05-13 08:47:36 -0600

    Save work

--- spec/mailers/user_activation_spec.rb
+++ spec/mailers/user_activation_spec.rb
@@ -1,5 +1,44 @@
 require "rails_helper"
 
 RSpec.describe UserActivationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'test' do
+
+  # pending "add some examples to (or delete) #{__FILE__}"
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up now.'
+
+    expect(current_path).to eq(new_user_path)
+
+    tester = {
+      email: 'jimbob@aol.com',
+      first_name: 'Jim',
+      last_name: 'Bob',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+
+    fill_in 'user[email]', with: tester[:email]
+    fill_in 'user[first_name]', with: tester[:first_name]
+    fill_in 'user[last_name]', with: tester[:last_name]
+    fill_in 'user[password]', with: tester[:password]
+    fill_in 'user[password_confirmation]', with: tester[:password]
+
+    click_on'Create Account'
+
+    expect(@user.status).to eql('inactive')
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content("Logged in as #{@user.first_name}")
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
+
+  # Add Magic Email stuff
+
+    expect(@user.status).to eql('active')
+    expect(page).to have_content("Thank you! Your account is now activated.")
+  end
 end
