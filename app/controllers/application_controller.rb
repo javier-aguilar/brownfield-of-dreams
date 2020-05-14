e927f4a9c9836e44f24cc78e18d53a91e3ffd359
Javier Aguilar <jvr.agx@gmail.com>
2020-05-13 21:48:29 -0600

    Add require_current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,9 @@ class ApplicationController < ActionController::Base
   def four_oh_four
     raise ActionController::RoutingError, 'Not Found'
   end
+
+  def require_current_user
+    four_oh_four unless current_user
+  end
+
 end
